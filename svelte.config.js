28ec37a6237beacdd21b0f818ea30767f1a21182
Jorge Corzo <jorgeach111@hotmail.com>
2023-06-24 20:23:49 +0930

    .

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,21 @@
-import adapter from '@sveltejs/adapter-static';
 
-const dev = process.argv.includes('dev');
+import adapter from '@sveltejs/adapter-cloudflare';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
-        adapter: adapter(),
-        paths: {
-            base: dev ? '' : process.env.BASE_PATH,
-        }
-    }
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter({
+			routes: {
+                include: ['/*'],
+                exclude: ['<all>']
+            }
+        })
+	},
+	preprocess: vitePreprocess()
 };
+
+export default config;
